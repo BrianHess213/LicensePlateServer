6aa3b19f120d21febfdd896760af47dc2d754e6b
Brian Hess <109031060+BrianHess213@users.noreply.github.com>
2023-12-03 18:33:21 -0700

    Going to push alot of commit trying to get my routes to work with Railway

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'https://license-plate-fl7yeb3ob-brianhess213.vercel.app/'
+  origin: 'licenseplateserver-production.up.railway.app'
 }));
 
 // Global MongoClient variable
